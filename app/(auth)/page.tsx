3eba31a08116562cda2966e7e28c149a96eb89f0
Kuldeep Jadeja <kuldeepsinh.jadeja@thegatewaycorp.co.in>
2026-04-03 17:03:54 +0530

    update repository link in the auth page

--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -30,7 +30,7 @@ const page = () => {
               </span>
             </Link>
             <Link
-              href="https://github.com/exclusiveabhi/ai-resume-builder.git"
+              href="https://github.com/kuldeep71088/ResumeX.git"
               className="relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:border before:border-transparent before:bg-slate-200 before:bg-gradient-to-b before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max"
             >
               <span className="relative text-base font-semibold text-primary">
